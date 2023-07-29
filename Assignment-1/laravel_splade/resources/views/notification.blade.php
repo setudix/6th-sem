ae4d2cc5c34b88eeed259006cc72184e786ea966
setu <setu@setu.com>
2023-07-30 03:36:04 +0600

    view button in notification page

--- Assignment-1/laravel_splade/resources/views/notification.blade.php
+++ Assignment-1/laravel_splade/resources/views/notification.blade.php
@@ -6,12 +6,16 @@
         <div class="py-4">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="p-6 bg-white border-gray-200">
                         {{ $notification['data']['message'] }}
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                            <Link modal href="{{ route('dashboard.show', $notification['data']['post_id']) }}">
+                            View
+                            </Link>
+                        </button>
                     </div>
-                    <Link modal href="{{ route('dashboard.show', $notification['data']['post_id'] ) }}">
-                    View
-                    </Link>
+                    <div class="pl-6 pt-4 pb-4 bg-white">
+                    </div>
                 </div>
             </div>
         </div>
