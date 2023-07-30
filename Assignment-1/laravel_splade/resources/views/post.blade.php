c4f9b5f02eabc0705ada6e3e8ee80742b735aeb4
setu <setu@setu.com>
2023-07-30 20:37:20 +0600

    post modal bug fixed

--- Assignment-1/laravel_splade/resources/views/post.blade.php
+++ Assignment-1/laravel_splade/resources/views/post.blade.php
@@ -7,7 +7,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <x-post-card>
+                    <x-post-card :photos="$post->photos">
                         <x-slot name="content"> {{ $post->content }} </x-slot>
                         <x-slot name="name"> {{ $post->user->name }} </x-slot>
                         <x-slot name="created_at"> {{ $post->created_at }} </x-slot>
